5ed33fa80e8be1e98a3d225d108939388522f145
gavinti <tim.gavin@live.fr>
2021-05-18 22:14:52 +0100

    added device

--- DQN.py
+++ DQN.py
@@ -7,8 +7,9 @@ import torchvision.transforms as T
 
 class DQN(nn.Module):
     # Defining and initializing the architecture of the NN
-    def __init__(self, window_size, num_actions):
+    def __init__(self, device, window_size, num_actions):
         super().__init__()
+        self.device = device
         # Note that the input to the NN is the flattened tensor of the grid state (here, 1 x 25)
         self.fc1 = nn.Linear(window_size ** 2, 16)
         self.fc2 = nn.Linear(16, 8)
@@ -17,6 +18,7 @@ class DQN(nn.Module):
     # Defining an implicit function to carry out the forward pass of the network
     def forward(self, x):
         # TODO: see for using gpu
+        x = x.to(self.device)
         x = F.relu(self.fc1(x))  # Applying a relu activation to the 1st FC layer (input)
         x = F.relu(self.fc2(x))  # The same for the hidden layer
         return self.fc3(x)  # Returning the 'Q' values for each of the possible actions
