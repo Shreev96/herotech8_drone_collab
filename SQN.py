c7859895c8b678e10955e145ed4be551f653cb19
Shreevanth Gopalakrishnan <77167062+Shreev96@users.noreply.github.com>
2021-06-10 16:43:08 +0100

    Added Conv_SQN as discussed to deal with dict-dimensional agent state input. Other minor changes.

--- SQN.py
+++ SQN.py
@@ -7,9 +7,11 @@ import torch.nn.functional as F
 from agent import AgentBase
 
 
-class SoftQNetwork(nn.Module):
+class SQN(nn.Module):
+    """Simple DNN"""
+
     def __init__(self, window_size, num_actions):
-        super(SoftQNetwork, self).__init__()
+        super(SQN, self).__init__()
         # Note that the input to the NN is the flattened tensor of the grid state (here, 1 x window_size ** 2)
         self.fc1 = nn.Linear(window_size ** 2, 64)
         self.relu = nn.ReLU()
@@ -23,14 +25,68 @@ class SoftQNetwork(nn.Module):
         return x
 
 
+class Conv_SQN(nn.Module):
+    # Convolutional version of the SQN
+
+    def __init__(self, window_size, num_actions, device, dict_size):
+
+        super(Conv_SQN, self).__init__()
+
+        self.window_size = window_size  # (square root) Size of the grid that the network sees
+        self.device = device
+
+        self.kernel_size = (3, 3)  # Square kernel
+        self.stride = (1, 1)  # Equal and continuous stride size due to small grid
+
+        self.conv1 = nn.Conv2d(dict_size, dict_size*2, kernel_size=self.kernel_size, stride=self.stride)
+        self.conv2 = nn.Conv2d(dict_size*2, 32, kernel_size=self.kernel_size, stride=self.stride)
+
+        def conv2d_size_out(shape, kernel_size=(3, 3), stride=(1, 1)):
+            """Size of array after convolution(s). Refer to Pytorch website"""
+
+            h = (shape[0] - (kernel_size[0] - 1) - 1)//stride[0] + 1  # height of convolved array
+            w = (shape[1] - (kernel_size[1] - 1) - 1)//stride[1] + 1  # width of convolved array
+
+            shape_out = (h, w)
+
+            return shape_out
+
+        # Apply twice since 2x filtering stages
+        conv_size = conv2d_size_out(conv2d_size_out((window_size, window_size), self.kernel_size, self.stride),
+                                    self.kernel_size, self.stride)
+
+        linear_input_size = conv_size[0] * conv_size[1] * 32  # 32 is the filter dimension
+
+        # Linear layer to convert the convolved grid to actions
+        self.head = nn.Linear(linear_input_size, num_actions)
+
+    # Called with either one element to determine next action, or a batch
+    # during optimization. Returns tensor([[left0exp,right0exp]...]).
+    def forward(self, x):
+
+        # x = x.to(self.device)
+        # x = torch.unsqueeze(x, 1)  # Add a batch dimension
+
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+
+        x = x.view(x.shape[0], -1)  # Equivalent to flattening the tensor
+
+        return self.head(x)  # Returning the 'Q' values for each of the possible actions
+
+
 class AgentSQN(AgentBase):
 
     def __init__(self, i, window_size, device, start=None, goal=None):
         super().__init__(i, device, start, goal)
 
         # SQL stuff
-        self.policy_model = SoftQNetwork(window_size=window_size, num_actions=len(self.actions)).to(self.device)
-        self.target_model = SoftQNetwork(window_size=window_size, num_actions=len(self.actions)).to(self.device)
+        self.policy_model = Conv_SQN(device=self.device, dict_size=len(self.GridLegend),
+                                     window_size=window_size, num_actions=len(self.actions)).to(self.device)
+
+        self.target_model = Conv_SQN(device=self.device, dict_size=len(self.GridLegend),
+                                     window_size=window_size, num_actions=len(self.actions)).to(self.device)
+
         self.target_model.load_state_dict(self.policy_model.state_dict())
 
         # Entropy weighting
@@ -52,7 +108,10 @@ class AgentSQN(AgentBase):
         self.optimizer = optim.Adam(self.policy_model.parameters(), lr=self.learning_rate)
 
     def select_action(self, observation):
-        observation = torch.FloatTensor(observation).view(1, -1).to(self.device)
+
+        # Shape of observation is (N_batch, Channels_in, Height, Width)
+        observation = torch.FloatTensor(observation).to(self.device)  # No longer need to flatten it
+
         with torch.no_grad():
             # Compute soft Action-Value Q values
             q_values = self.policy_model(observation)
@@ -63,7 +122,7 @@ class AgentSQN(AgentBase):
             dist = dist / torch.sum(dist)
             c = Categorical(dist)
             a = c.sample()
-        return a.view(1, 1)
+        return a.view(1,)  # Want a 1D tensor returned
 
     def train(self):
         if len(self.experience_replay) < self.batch_size:
@@ -91,7 +150,8 @@ class AgentSQN(AgentBase):
         non_final_next_states = torch.cat(batch.next_state)[non_final_mask].to(self.device)
 
         # Compute Q(s_t, a)
-        state_action_values = self.policy_model(state_batch).gather(1, action_batch.long())
+        state_action_values = self.policy_model(state_batch)\
+            .gather(1, action_batch.long().reshape(len(action_batch), 1))
 
         # Compute the expected values
         with torch.no_grad():
