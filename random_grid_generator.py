dc1713a2e088ca0a677f9736a41a444c76856d93
Shreevanth Gopalakrishnan <77167062+Shreev96@users.noreply.github.com>
2021-06-10 16:42:10 +0100

    Works better with 10x10 grids now. find_neighbours method added.

--- random_grid_generator.py
+++ random_grid_generator.py
@@ -1,7 +1,33 @@
 import numpy as np
 
 
-def random_maze(width=81, height=51, complexity=.75, density=.75):
+def find_neighbours(origin, max_y, max_x, min_x=0, min_y=0):
+    """Given a position on the grid, this function is used to determine its neighbours"""
+
+    neighbours = []
+
+    (y, x) = origin
+
+    # Check above
+    if y > min_y:
+        neighbours.append((y - 1, x))
+
+    # Check below
+    if y < max_y - 1:
+        neighbours.append((y + 1, x))
+
+    # Check left
+    if x > min_x:
+        neighbours.append((y, x - 1))
+
+    # Check right
+    if x < max_x - 1:
+        neighbours.append((y, x + 1))
+
+    return neighbours
+
+
+def random_maze(start, goal, width=10, height=10, complexity=.75, density=.75):
     """Generate a random maze array.
 
     It only contains two kind of objects, obstacle and free space. The numerical value for obstacle
@@ -11,55 +37,44 @@ def random_maze(width=81, height=51, complexity=.75, density=.75):
     https://github.com/zuoxingdong/mazelab/blob/master/mazelab/generators/random_maze.py
     """
 
-    # Only odd shapes
-    shape = ((height // 2) * 2 + 1, (width // 2) * 2 + 1)
+    shape = (height, width)  # Shape of the grid
 
     # Adjust complexity and density relative to maze size
-    complexity = int(complexity * (5 * (shape[0] + shape[1])))
-    density = int(density * ((shape[0] // 2) * (shape[1] // 2)))
+    complexity = int(complexity * (shape[0] + shape[1])/2)
+    density = int(density * ((shape[0] // 3) * (shape[1] // 3)))
 
-    # Build actual maze
-    Z = np.ones(shape, dtype=bool)
+    # Finding the neighbours of the goal state and the start state (can be anywhere)
+    neighbours_start = find_neighbours(start, height, width)
+    neighbours_end = find_neighbours(goal, height, width)
 
-    # Fill borders
-    Z[0, :] = Z[-1, :] = 0
-    Z[:, 0] = Z[:, -1] = 0
+    neighbours_ = neighbours_start + neighbours_end  # Joining both lists
 
-    # Make aisles
-    for i in range(density):
-        x, y = np.random.randint(0, shape[1] // 2 + 1) * 2, np.random.randint(0, shape[0] // 2 + 1) * 2
-        Z[y, x] = 0
+    while True:
 
-        for j in range(complexity):
-            neighbours = []
+        # Build actual maze
+        Z = np.ones(shape, dtype=bool)
 
-            if x > 1:
-                neighbours.append((y, x - 2))
+        # Make aisles
+        for i in range(density):
 
-            if x < shape[1] - 2:
-                neighbours.append((y, x + 2))
+            y, x = np.random.randint(2, shape[0] - 3), np.random.randint(2, shape[1] - 3)
 
-            if y > 1:
-                neighbours.append((y - 2, x))
+            Z[y, x] = 0
 
-            if y < shape[0] - 2:
-                neighbours.append((y + 2, x))
+            for j in range(complexity):
+                neighbours = find_neighbours((y, x), height, width)
 
-            if len(neighbours):
-                y_, x_ = neighbours[np.random.randint(0, len(neighbours))]
+                if len(neighbours):
+                    y_, x_ = neighbours[np.random.randint(0, len(neighbours))]
 
-                if Z[y_, x_] == 1:
-                    Z[y_, x_] = 0
-                    Z[y_ + (y - y_) // 2, x_ + (x - x_) // 2] = 0
-                    x, y = x_, y_
+                    if Z[y_, x_] == 1:
+                        Z[y_, x_] = 0
+                        Z[y_ + (y - y_) // 2, x_ + (x - x_) // 2] = 0
+                        x, y = x_, y_
 
-    # # Trimming off the borders
+        # Start state, goal state and at least one neighbour of each must be free
+        if Z[goal] == 1 and Z[start] == 1 and any(Z[neighbour] == 1 for neighbour in neighbours_):
+            break
+
     grid = Z.astype(float)
-    grid = grid[1:width, 1:height]
-
     return grid
-
-
-# # Example use
-# init_grid = random_maze(width=6, height=6, complexity=0.50, density=0.50)
-# print(init_grid)
