cc733150906b6668119ae0c272751246623d4951
Shreevanth Gopalakrishnan <77167062+Shreev96@users.noreply.github.com>
2021-06-18 02:40:55 +0100

    Added the ability to account for wind (including reading the config file); 'improvement' metric plot.

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import numpy as np
 import torch
 
 from SQN import AgentSQN
+from DQN_Conv import AgentDQN
+
 from gridworld import GridWorld
 from grid_generators.random_grid_generator import random_maze
 from grid_generators.random_start_end import random_start_end
@@ -26,8 +28,9 @@ def read_config(config_file):
     episodes = rl_parameters.getint("episodes")
     train_period = rl_parameters.getint("train_period")
     start_goal_reset_period = rl_parameters.getint("start_goal_reset_period")
+    effects = rl_parameters.getboolean("effects")
 
-    return steps, episodes, train_period, start_goal_reset_period
+    return steps, episodes, train_period, start_goal_reset_period, effects
 
 
 def read_grid_config(config_file):
@@ -44,9 +47,26 @@ def read_agents_config(config_file):
     grid_size = config.getint("Grid Parameters", "grid_size")
 
     if model == "SQN":
-        return [AgentSQN(i, window_size=grid_size, device=DEVICE) for i in range(n)]
+        return [AgentSQN(i, window_size=grid_size, device=DEVICE) for i in range(n)], model
+    elif model == "DQN":
+        return [AgentDQN(i, window_size=grid_size, device=DEVICE) for i in range(n)], model
     else:
         raise NotImplementedError
+
+
+def read_env_config(config_file):
+
+    # Environmental conditions to be considered
+
+    config = configparser.ConfigParser(allow_no_value=True)
+    config.read(config_file)
+
+    env_parameters = config['Environmental Effects']
+    col_wind = env_parameters.getint("col_wind")
+    range_random_wind = env_parameters.getint("range_random_wind")
+    probabilities = env_parameters.getint("probabilities")
+
+    return col_wind, range_random_wind, probabilities
 
 
 def create_gif(filename, env, init_grid, steps=100, epidodes=1):
@@ -113,7 +133,7 @@ def create_gif2(filename, env, init_grid, steps=100, epidodes=1):
 if __name__ == '__main__':
     print(DEVICE)
 
-    steps, episodes, train_period, start_goal_reset_period = read_config("config.ini")
+    steps, episodes, train_period, start_goal_reset_period, effects = read_config("config.ini")
     step_done = 0
 
     #################
@@ -141,28 +161,28 @@ if __name__ == '__main__':
     # agent_1 = AgentSQN(2, window_size=grid_size, device=DEVICE)
     # agent_1 = AgentSQN(2, window_size=5, device=device, start=start_position)
     # agent_1 = AgentSQN(2, window_size=5, device=device, goal=goal)
-    agents = read_agents_config("config.ini")
+    agents, model = read_agents_config("config.ini")
 
     #######################
     # GRIDWORLDS CREATION #
     #######################
-    # # Ordinary gridworld
-    env = GridWorld(agents=agents, grid=init_grid)
+    if not effects:
+        # Ordinary gridworld
+        env = GridWorld(agents=agents, grid=init_grid)
+
+    else:
+        # Stochastic windy gridworld
+        col_wind, range_random_wind, probabilities = read_env_config("config.ini")
+        env = GridWorld(agents=agents, grid=init_grid, col_wind=col_wind,
+                        range_random_wind=range_random_wind, probabilities=probabilities)
+
     env.read_reward_config("config.ini")
-
-    # # Stochastic windy gridworld
-    # wind = [0, 1, 1, 0, 0]  # Row-wise wind addition towards the right (increasing col. idx)
-    # range_random_wind = 1
-    # probabilities = [0.2, 0.3, 0.5]  # [-1, 0, 1] noise added to wind-related position updates
-
-    # env = GridWorld(agents=[agent_1], grid=init_grid, col_wind=wind,
-    #                 range_random_wind=range_random_wind, probabilities=probabilities)
 
     ########
     # MAIN #
     ########
 
-    # plt.ion()
+    plt.ion()
 
     cum_rewards = []
     total_steps = []
@@ -225,28 +245,43 @@ if __name__ == '__main__':
         env.save(directory="logs/models", datetime=now.strftime('%Y%m%d%H%M%S'))
 
         plt.clf()
-        plt.title("Cumulated Reward")
+        plt.title(("Cumulated Reward for " + model))
         plt.xlabel("Epochs")
         plt.plot(cum_rewards)
         plt.savefig(f"logs/cumulated_rewards/{now.strftime('%Y%m%d%H%M%S')}.png")
         plt.clf()
 
-        plt.title("Total steps")
+        plt.title(("Total steps for " + model))
         plt.xlabel("Epochs")
         plt.plot(total_steps)
         plt.savefig(f"logs/total_steps/{now.strftime('%Y%m%d%H%M%S')}.png")
         plt.clf()
 
-    # plt.figure()
-    # # evaluate value function for agent 1 (id = 0) and display
-    # v_values = env.eval_value_func(0)
-    # env.render(value_func=True, v_values=v_values)
-    # plt.show()
-    #
-    # # devise a greedy deterministic policy for agent 1 and display
-    # U, V = env.greedy_det_policy(v_values, 0)  # U and V are x and y unit vectors respectively
-    # env.render(policy=True, u=U, v=V)
-    # plt.show()
+        # Calculate improvement over training duration
+        # Equivalent to the # of times the agent reaches the goal within the designated number of timesteps
+        improvement = np.zeros_like(total_steps)
+        counter = 0
+        for ii in range(len(total_steps)):
+            if total_steps[ii] < steps - 1:
+                counter += 1
+            improvement[ii] = counter
+
+        plt.title(("Improvement over training episodes for " + model))
+        plt.xlabel("Epochs")
+        plt.plot(improvement)
+        plt.savefig(f"logs/improvement/{now.strftime('%Y%m%d%H%M%S')}.png")
+        plt.clf()
+
+        plt.figure()
+        # evaluate value function for agent 1 (id = 0) and display
+        v_values = env.eval_value_func(0, model)
+        env.render(value_func=True, v_values=v_values)
+        plt.show()
+
+        # devise a greedy deterministic policy for agent 1 and display
+        U, V = env.greedy_det_policy(v_values, 0)  # U and V are x and y unit vectors respectively
+        env.render(policy=True, u=U, v=V)
+        plt.show()
 
     start_time = time.time()
     create_gif("test", env, init_grid, steps=10, epidodes=3)
